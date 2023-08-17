ad6d442524d55c5a0656047925dc3c3923718046
p0lygun <solankivibhakar82@gmail.com>
2023-08-17 14:32:04 +0530

    feat(qualifier): complete `valid_input` funciton

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,13 +1,31 @@
-def valid_input(image_size: tuple[int, int], tile_size: tuple[int, int], ordering: list[int]) -> bool:
+import numpy as np
+
+
+def valid_input(
+    image_size: tuple[int, int], tile_size: tuple[int, int], ordering: list[int]
+) -> bool:
     """
     Return True if the given input allows the rearrangement of the image, False otherwise.
 
-    The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
-    once.
+    The tile size must divide each image dimension without remainders, 
+    and `ordering` must use each input tile exactly once.
+
     """
+    if image_size[0] % tile_size[0] != 0 or image_size[1] % tile_size[1] != 0:
+        return False
+
+    if not len(set(ordering)) == len(ordering):  # check for duplicates
+        return False
+
+    if ((image_size[0] * image_size[1]) // (tile_size[0] * tile_size[1])) != len(ordering):
+        return False
+
+    return True
 
 
-def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[int], out_path: str) -> None:
+def rearrange_tiles(
+    image_path: str, tile_size: tuple[int, int], ordering: list[int], out_path: str
+) -> None:
     """
     Rearrange the image.
 
